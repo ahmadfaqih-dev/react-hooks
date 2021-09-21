eef87ecf2da38cd9511f274a8bbbfba4ed869218
ahmadfaqih-dev <ahmadfaqih.dev@gmail.com>
2021-09-21 22:50:28 +0700

    Dinamic Values

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 function App() {
+  const channel = "Website Tutorial Reactjs";
+  const age = 20;
+  const link = "https://ahmadfaqih-dev.github.io/";
+
   return (
     <div>
-      <h1>Welcome</h1>
+      <h1>Welcome to {channel}</h1>
+      <h2>{age * 2}</h2>
+      <a href={link}>My Website</a>
     </div>
   );
 }
