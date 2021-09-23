867ccca778cd778a24e4a7beddffcb0ebfbe0346
ahmadfaqih-dev <ahmadfaqih.dev@gmail.com>
2021-09-23 16:25:07 +0700

    Click Events

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ function App() {
   const age = 20;
   const link = "https://ahmadfaqih-dev.github.io/";
 
+  const clickMe = (name) => {
+    console.log('Hello'+ name);
+  }
+
   return (
     <div>
       <Header />
       <h1>Welcome to {channel}</h1>
       <h2>{age * 2}</h2>
       <a href={link}>My Website</a>
+      <button onClick={ () => clickMe ('Faqih') }>Click Me</button>
     </div>
   );
 }
