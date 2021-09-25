eba78e6ea8ec85c1d36d770df7021a059b4e1f44
ahmadfaqih-dev <ahmadfaqih.dev@gmail.com>
2021-09-25 11:17:12 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import { useState  } from 'react';
 import Header from './components/Header'
 
 function App() {
-  const channel = "Website Tutorial Reactjs";
-  const age = 20;
+  const [channel, setChannel] = useState("Website Tutorial Reactjs");
+  const [age, setAge] = useState(20);
   const link = "https://ahmadfaqih-dev.github.io/";
 
   const clickMe = (name) => {
     console.log('Hello'+ name);
   }
 
+  const changeChannel = () => {
+    setChannel("React JS Dasar");
+    setAge(25);
+  }
+
   return (
     <div>
       <Header />
       <h1>Welcome to {channel}</h1>
-      <h2>{age * 2}</h2>
+      <h2>Umur: {age}</h2>
       <a href={link}>My Website</a>
       <button onClick={ () => clickMe ('Faqih') }>Click Me</button>
+      <button onClick={changeChannel}>Change Titile</button>
     </div>
   );
 }
